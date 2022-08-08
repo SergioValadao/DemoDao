2fca0898bf47bd8a35aceff82f8f218847321fe5
SergioValadao <sergiovaladao@gmail.com>
2022-08-08 19:54:10 -0300

    Classes Departamento e Vendedor entities

--- src/Aplication/Main.java
+++ src/Aplication/Main.java
@@ -1,9 +1,18 @@
 package Aplication;
 
+import java.util.Date;
+
+import model.entities.Departamento;
+import model.entities.Vendedor;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Departamento obj = new Departamento(1, "Books");
+		
+		Vendedor vendedor = new Vendedor(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(vendedor);
 
 	}
 
